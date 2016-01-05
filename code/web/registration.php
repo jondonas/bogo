8318f52de76f559da73f2e017ccd63d815582e9d
Austin <austingomez@hotmail.ca>
2016-01-05 18:00:10 -0500

    CSS changes, centering

--- code/web/registration.php
+++ code/web/registration.php
@@ -2,6 +2,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/menu.css">
+    <link rel="stylesheet" type="text/css" href="css/registration.css">
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,500' rel='stylesheet' type='text/css'>
 <title>Register</title>
 </head>
@@ -15,22 +16,21 @@
             <li><a href="about.html" title="About">About</a> </li>                
             </ul>
      </div>
+    
+    <div class="forms">
      <form id="forms" method="post" action="php/send.php">
-    <div class="field">
-        <label for="firstname">First Name:</label>
+        <p>First Name:</p>
         <input type="text" id="firstname" name="firstname" required>
-    </div>
-    <div class="field">
-        <label for="lastname">Last Name:</label>
+   
+        <p>Last Name:</p>
         <input type="text" id="firstname" name="lastname" required>
-    </div>
-    <div class="field">
-        <label for="email">Email:</label>
+   
+        <p>Email:</p>
         <input type="text" id="firstname" name="email" required>
-    </div>
-    <div class="field">
+ 
         <button id="submit-btn" type="submit">Submit</button>
-    </div>
+    
+         </div>
 </form>
 
 </body>
