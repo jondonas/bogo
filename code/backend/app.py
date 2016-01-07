29b955855f922c5dc103ca1b689800f18d97210e
root <root@ip-172-31-41-101.us-west-2.compute.com>
2016-01-07 15:18:21 +0000

    backend fix

--- code/backend/app.py
+++ code/backend/app.py
@@ -1,14 +1,44 @@
 import random
 import datetime
 import time
-from mailer import mailAll
+import MySQLdb
+import smtplib
+
+def mail():
+
+    SERVER = "localhost"
+    FROM = "austin@bogo.gq"
+    TO = []
+
+    db = MySQLdb.connect(host='localhost', user='root', passwd='paper', db='bogo')
+    cursor = db.cursor()
+    cursor.execute('SELECT email FROM users WHERE email IS NOT NULL')
+    db.commit()
+    rows = cursor.fetchall()
+    for user in rows:
+        TO.append(user)
+
+    SUBJECT = "It's Done!"
+    TEXT = "Get ready to party!"
+
+    message = """\
+                From: %s
+                To: %s
+                Subject %s
+                
+    %s
+    """ % (FROM, ", ".join(''.join(elems) for elems in TO), SUBJECT, TEXT)
+
+    server = smtplib.SMTP(SERVER)
+    server.sendmail(FROM, TO, message)
+    server.quit()
 
 #basic bogosort example
-list1 = [1, 2, 3, 4, 5, 6, 7, 8]
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 def unordered(alist):
-    while alist != [1, 2, 3, 4, 5, 6, 7, 8]:
+    while alist != [1, 2, 3, 4, 5, 6, 7, 8, 9]:
         return True
     return False
 
@@ -18,13 +48,13 @@ def bogosort(somelist):
     print "running"
     a = 0
     while unordered(somelist):
-	time.sleep(0.01)
+	time.sleep(6.5)
         random.shuffle(somelist)
         print a
         a += 1
     print datetime.datetime.now() - start
     print "BOGONIGHT"
-    mailer.mailAll() 
+    mail() 
 
 random.shuffle(list1)
 bogosort(list1)
